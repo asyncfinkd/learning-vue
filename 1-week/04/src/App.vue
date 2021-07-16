0fd71cb687189fe9924a107cbfbaa0fc326fa8f2
Nika Shamiladze <shamgoingzuck@gmail.com>
2021-07-16 18:23:46 +0400

    Props

--- 1-week/04/src/App.vue
+++ 1-week/04/src/App.vue
@@ -1,17 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <Message messageText="Hello This is vue new project" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Message from "./components/Message.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Message,
+  },
+};
 </script>
 
 <style>
---
+++ 1-week/04/src/components/Message.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>{{ messageText }}</h1>
+</template>
+
+<script>
+export default {
+  name: "Message",
+  props: {
+    messageText: String,
+  },
+};
+</script>
